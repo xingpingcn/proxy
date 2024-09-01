ac74092f86da56407ff9481bde4979bc02bb8d53
xingpingcn <117259690+xingpingcn@users.noreply.github.com>
2024-09-01 19:08:03 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
     let url = new URL(request.url);
     if (url.pathname.startsWith('/')) {
-      url.hostname = 'www.google.com.hk/search'
+      url.hostname = 'www.google.com.hk'
       let new_request = new Request(url, request);
       return fetch(new_request);
     }
